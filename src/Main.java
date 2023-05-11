edda63eaea7feb2fc4bbad36771e01683507394b
VovaZ <zalikvova1998@gmail.com>
2023-05-11 15:26:55 +0300

    lambda

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args) {
         List<Person> persons = new ArrayList<>(generatePersons());
 
-        Collections.sort(persons, new PersonsComparator(6));
+        Collections.sort(persons, (o1, o2) -> {
+            if (o1.getSurname().length() > 6 && o2.getSurname().length() > 6) {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }
+            else return Integer.compare(o1.getSurname().length(), o2.getSurname().length());
+        });
+
         System.out.println(persons);
     }
     public static List<Person> generatePersons() {
